7593017ec859d58f0bcfa9c73ed69137c0807b3d
oscartse68 <44157508+oscartse68@users.noreply.github.com>
2020-05-28 19:01:09 +0800

    Delete quality_check_viator.py

--- viator_scrap/quality_check_viator.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("/Users/oscartse/Desktop/Klook_WebScrappingProject/viator_scrap/viator_scrap/spiders/records.csv")
-counter = 0
-for i in df["count"]:
-    counter+=i
-print(counter)
