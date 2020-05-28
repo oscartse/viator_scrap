b180ca316c382118a0882c4964a9fb22221c9508
oscartse68 <44157508+oscartse68@users.noreply.github.com>
2020-05-28 19:00:36 +0800

    Update settings.py

--- viator_scrap/settings.py
+++ viator_scrap/settings.py
@@ -58,34 +58,31 @@ DOWNLOADER_MIDDLEWARES = {
    # 'scrapy_useragents.downloadermiddlewares.useragents.UserAgentsMiddleware': 500,
    # 'rotating_proxies.middlewares.RotatingProxyMiddleware': 610,
 }
-# USER_AGENTS = [
-#     ('Mozilla/5.0 (X11; Linux x86_64) '
-#      'AppleWebKit/537.36 (KHTML, like Gecko) '
-#      'Chrome/57.0.2987.110 '
-#      'Safari/537.36'),  # chrome
-#     ('Mozilla/5.0 (X11; Linux x86_64) '
-#      'AppleWebKit/537.36 (KHTML, like Gecko) '
-#      'Chrome/61.0.3163.79 '
-#      'Safari/537.36'),  # chrome
-#     ('Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:55.0) '
-#      'Gecko/20100101 '
-#      'Firefox/55.0'),  # firefox
-#     ('Mozilla/5.0 (X11; Linux x86_64) '
-#      'AppleWebKit/537.36 (KHTML, like Gecko) '
-#      'Chrome/61.0.3163.91 '
-#      'Safari/537.36'),  # chrome
-#     ('Mozilla/5.0 (X11; Linux x86_64) '
-#      'AppleWebKit/537.36 (KHTML, like Gecko) '
-#      'Chrome/62.0.3202.89 '
-#      'Safari/537.36'),  # chrome
-#     ('Mozilla/5.0 (X11; Linux x86_64) '
-#      'AppleWebKit/537.36 (KHTML, like Gecko) '
-#      'Chrome/63.0.3239.108 '
-#      'Safari/537.36'),  # chrome
-# ]
-# ROTATING_PROXY_LIST = [
-#    "http://lum-customer-klook-zone-shared_data_center:hja29x3mhtyy@zproxy.lum-superproxy.io:22225",
-# ]
+USER_AGENTS = [
+    ('Mozilla/5.0 (X11; Linux x86_64) '
+     'AppleWebKit/537.36 (KHTML, like Gecko) '
+     'Chrome/57.0.2987.110 '
+     'Safari/537.36'),  # chrome
+    ('Mozilla/5.0 (X11; Linux x86_64) '
+     'AppleWebKit/537.36 (KHTML, like Gecko) '
+     'Chrome/61.0.3163.79 '
+     'Safari/537.36'),  # chrome
+    ('Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:55.0) '
+     'Gecko/20100101 '
+     'Firefox/55.0'),  # firefox
+    ('Mozilla/5.0 (X11; Linux x86_64) '
+     'AppleWebKit/537.36 (KHTML, like Gecko) '
+     'Chrome/61.0.3163.91 '
+     'Safari/537.36'),  # chrome
+    ('Mozilla/5.0 (X11; Linux x86_64) '
+     'AppleWebKit/537.36 (KHTML, like Gecko) '
+     'Chrome/62.0.3202.89 '
+     'Safari/537.36'),  # chrome
+    ('Mozilla/5.0 (X11; Linux x86_64) '
+     'AppleWebKit/537.36 (KHTML, like Gecko) '
+     'Chrome/63.0.3239.108 '
+     'Safari/537.36'),  # chrome
+]
 # Enable or disable extensions
 # See https://doc.scrapy.org/en/latest/topics/extensions.html
 #EXTENSIONS = {
